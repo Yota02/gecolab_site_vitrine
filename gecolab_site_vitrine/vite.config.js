f41a5f8fe9ce57b1dc01c9b1cfe1942a2abfe530
michauxa <alexis.michaux-kinet@etu.umontpellier.fr>
2026-02-24 15:27:49 +0100

    fix: kqjlkqjljlkjljlkj

--- gecolab_site_vitrine/vite.config.js
+++ gecolab_site_vitrine/vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
-  base: '/gecolab_site_vitrine/'
+  base: '/gecolab_site_vitrine/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  }
 })
